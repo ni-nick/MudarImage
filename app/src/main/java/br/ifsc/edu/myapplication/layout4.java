7c32f94d6aec7a7bfbc9a459891f04615478bf34
Samantha <samantha.gonzaga.locatelli@hotmail.com>
2020-03-05 11:24:11 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/myapplication/layout4.java
@@ -0,0 +1,14 @@
+package br.ifsc.edu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout4);
+    }
+}
