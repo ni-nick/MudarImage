4e09936c6468479c13c93a8a84122079c94cf63d
Samantha <samantha.gonzaga.locatelli@hotmail.com>
2020-03-12 11:13:25 -0300

    Mudando imagens pelo botão

--- app/src/main/java/br/ifsc/edu/myapplication/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/myapplication/MainActivity.java
@@ -6,24 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    List<Integer>lista_imagens;
+    int posicaoImagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
-    }
-
-    public  void mudaTexto(View view){
-        imageView.setImageResource(R.drawable.cachorro);
-
-    }
-
-    public  void  mudaImage(View view){
-        imageView.setImageResource(R.drawable.porquinho);
+       lista_imagens = Arrays.asList(
+               R.drawable.cachorro,
+               R.drawable.happy
+               R.drawable.porquinho,
+               R.drawable.gardem,
+               R.drawable.patinho);
+       posicaoImagem=0;
 
     }
+
+    public void mudaTexto(View view){
+        if(posicaoImagem<=lista_imagens.size()-1){ //tamanho da lista -1
+            imageView.setImageResource(lista_imagens.get(posicaoImagem));
+        }else{
+            posicaoImagem=0;
+            imageView.setImageResource(lista_imagens.get(posicaoImagem));
+        }
+        posicaoImagem++;
+
+    }
+
 }
+
